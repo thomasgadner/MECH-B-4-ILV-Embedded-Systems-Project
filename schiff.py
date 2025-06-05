2d5a8fedfa2272877a8ca0eccb116f71743f0870
Roland Lezuo <roland.lezuo@embedded-solutions.at>
2025-06-05 12:14:26 +0200

    fixes #4

--- schiff.py
+++ schiff.py
@@ -464,6 +464,7 @@ def main(state_machine, args):
         print("0--------1---------2---------3---------4---------5---------6---------7---------8---------9---------|")
 
     while True:
+        tournament_result_char = 'a'
         try:
             state_machine.reset()
             our_field = Field()
@@ -479,8 +480,10 @@ def main(state_machine, args):
 
             if state_machine.we_won:
                 won += 1
+                tournament_result_char = 'w'
             else:
                 lost += 1
+                tournament_result_char = 'l'
 
         except RuntimeError as e:
             logging.error("Exception in game-loop, will reset the game state")
@@ -501,7 +504,7 @@ def main(state_machine, args):
             logging.debug("waiting 1 second")
             time.sleep(1)
         else:
-            print("w" if won else "l" if lost else "a", end="", flush=True)
+            print(tournament_result_char, end="", flush=True)
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(epilog="Ein Schiff im Hafen ist sicher, doch dafür werden Schiffe nicht gebaut")
